9384370dac0b7c5dcf5bfe7925be859c08a99c50
AdamJednacz <adamjednacz4@gmail.com>
2024-08-15 14:45:43 +0200

    django vercel5

--- project/project/settings.py
+++ project/project/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     # Dodaj inne katalogi tutaj, jeśli są potrzebne
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 MEDIA_URL = "/files/"
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
